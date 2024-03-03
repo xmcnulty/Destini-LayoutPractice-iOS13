95da8c466363003ce1cb76504ae0174f4558232a
Xavier <x.mcnulty@gmail.com>
2024-03-03 13:27:45 -0500

    Added story logic.

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: StoryChoice
+    let choice2: StoryChoice
+    
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
+        self.choice1 = StoryChoice(text: choice1, desintaion: choice1Destination)
+        self.choice2 = StoryChoice(text: choice2, desintaion: choice2Destination)
+    }
+    
+    func getChoices() -> [StoryChoice] {
+        return [choice1, choice2]
+    }
+}
---
+++ Destini-iOS13/Model/StoryChoice.swift
@@ -0,0 +1,14 @@
+//
+//  StoryChoice.swift
+//  Destini-iOS13
+//
+//  Created by Xavier McNulty on 3/3/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct StoryChoice {
+    let text: String
+    let desintaion: Int
+}
